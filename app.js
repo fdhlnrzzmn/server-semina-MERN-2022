e585859f530da826b3ed7efec7e0d2f3605675cb
fdhlnrzzmn <fdhlnrzzmn@gmail.com>
2022-07-29 01:07:31 +0700

    API Orders

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const talentsRouter = require('./app/api/v1//talents/router');
 const eventsRouter = require('./app/api/v1//events/router');
 const organizersRouter = require('./app/api/v1//organizers/router');
 const authCMSRouter = require('./app/api/v1//auth/router');
+const ordersRouter = require('./app/api/v1//orders/router');
+
 
 
 const v1 = '/api/v1/cms';
@@ -37,6 +39,8 @@ app.use(v1, talentsRouter);
 app.use(v1, eventsRouter);
 app.use(v1, organizersRouter);
 app.use(v1, authCMSRouter);
+app.use(v1, ordersRouter);
+
 
 app.use(notFoundMiddleware);
 app.use(handleErrorMiddleware);
